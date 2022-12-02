a4a76c717421bac5288756c5281752fb01183eff
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-12-02 21:56:09 +0000

    everything

--- main.cs
+++ main.cs
@@ -1,7 +1,27 @@
+// Created by: sophie
+// Created on: dec 2020
+//
+// This program does loops and math
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num1;
+        int sum = 0;
+
+        Console.WriteLine("this program calculates the sum of n natural numbers");
+
+        Console.Write("Enter a number: ");
+        num1 = Convert.ToInt32(Console.ReadLine()) + 1;
+
+        for (int count = 1; count < num1; count++)
+        {
+            sum += count;
+        }
+        Console.WriteLine("your answer is: " + sum + ".");
+        Console.WriteLine("\nDone.");
+    }
 }
